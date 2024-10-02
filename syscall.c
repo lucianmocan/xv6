9e4b1a918afc4a49f23b58414f9e7c311350a1cb
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2024-10-02 09:12:48 +0200

    add sys_lseek to syscall.c, syscall.h and sysfile.c:

--- syscall.c
+++ syscall.c
@@ -82,6 +82,7 @@ argstr(int n, char **pp)
   return fetchstr(addr, pp);
 }
 
+extern int sys_lseek(void);
 extern int sys_chdir(void);
 extern int sys_close(void);
 extern int sys_dup(void);
@@ -126,6 +127,7 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_lseek] 	sys_lseek
 };
 
 void
--- sysfile.c
+++ sysfile.c
@@ -442,3 +442,8 @@ sys_pipe(void)
   fd[1] = fd1;
   return 0;
 }
+
+int sys_lseek(void)
+{
+	return 0;
+}
